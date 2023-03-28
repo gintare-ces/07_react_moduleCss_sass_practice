b5d040069f0d8f1b456f81b459bbd6641bef7b02
gintare-ces <gintare.cesiuliene@gmail.com>
2023-03-28 11:37:10 +0300

    count down color classes

--- src/components/section/countDown/CountDownSection.jsx
+++ src/components/section/countDown/CountDownSection.jsx
@@ -4,14 +4,17 @@ import css from './CountDownSection.module.css'
 import SingleCountDown from './SingleCountDown';
 
 function CountDownSection() {
+
+    
+
   return (
     <section className={`container ${css.section}`}>
         <h2 className={css.title}>Count down every second to <span>Event</span></h2>
         <Grid cols={4}>
-            <SingleCountDown />
-            <SingleCountDown />
-            <SingleCountDown />
-            <SingleCountDown />
+            <SingleCountDown time='Days' value={5} circle={'violet'} />
+            <SingleCountDown time='Hours' value={5} circle={'sky'} />
+            <SingleCountDown time='Minutes' value={5} circle={'blue'} />
+            <SingleCountDown time='Seconds' value={5} circle={'green'} />
         </Grid>
     </section>
   )
--- src/components/section/countDown/SingleCountDown.jsx
+++ src/components/section/countDown/SingleCountDown.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import css from './SingleCountDown.module.css'
 
-function SingleCountDown() {
+function SingleCountDown(props) {
+
+    // i value paragraph paduoti reiksme is props
+    // value ir h3 elementams uzdeti klase pagal props
+    // cirkle elementui uzdeti klase pagal props
+    // let colorsClass = '';
+    // switch(props.circle) {
+    //     case 'violet':
+    //         colorsClass = css.violet;
+    //         break;
+    //     case 'sky':
+    //         colorsClass = css.sky;
+    //         break;
+    //     case 'blue':
+    //         colorsClass = css.blue;
+    //         break;
+    //     case 'green':
+    //         colorsClass = css.green;
+    // }
   return (
-    <div>SingleCountDown</div>
+    <div className={`${css.circle} ${css[props.circle]}`}>
+        <p className={css.value}>{props.value}</p>
+        <h3 className={css.time}>{props.time}</h3>
+    </div>
   )
 }
 
