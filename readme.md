4ba755b4b07ca1ca81b38bd4c64afc81688ebb48
gintare-ces <gintare.cesiuliene@gmail.com>
2023-03-29 09:51:36 +0300

    section

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # Module css practice
 
 url https://droitthemes.com/html/saasland/home-event.html
+
+
+## instal fontawesome
+
+1. `npm i font-awesome@^4`  - suinstaliuosim fontawesome versija
+2. itrauksim fontawesome css i main.jsx
--- src/components/section/conference/ConferenceSection.jsx
+++ src/components/section/conference/ConferenceSection.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import SectionTitle from '../../title/SectionTitle'
-import ConferenceButton from './ConferenceButton'
-import ConferenceCard from './ConferenceCard'
-
+import React from 'react';
+import SectionTitle from '../../title/SectionTitle';
+import ConferenceButton from './ConferenceButton';
+import ConferenceCard from './ConferenceCard';
+import css from './ConferenceSection.module.css';
 
 function ConferenceSection() {
   return (
-    <section>
-        <div className="container">
-            <SectionTitle title={'Schedule'} subtitle={'Conference schedule'}/>
-            <ConferenceButton />
-            <ConferenceCard />
-        </div>
+    <section className={css.section}>
+      <SectionTitle title={'Schedule'} subtitle={'Conference schedule'} />
+      <div className="container">
+        <ConferenceButton />
+        <ConferenceCard />
+      </div>
     </section>
-  )
+  );
 }
 
-export default ConferenceSection
+export default ConferenceSection;
